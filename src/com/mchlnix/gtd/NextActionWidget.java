38dcabe3b028db74b29871611b0afe7539e8b31f
Michael Nix <mchl.nix@googlemail.com>
2015-05-10 11:37:31 +0200

    adds widgets

---
+++ src/com/mchlnix/gtd/NextActionWidget.java
@@ -0,0 +1,42 @@
+package com.mchlnix.gtd;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.text.style.TextAppearanceSpan;
+import android.view.View;
+import android.webkit.WebSettings.TextSize;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class NextActionWidget extends LinearLayout {
+
+	public NextActionWidget(Context context, String text ) {
+		super(context);
+		
+		this.setOrientation( LinearLayout.HORIZONTAL );
+		
+		TextView action = new TextView( context );
+		action.setText( text );
+		action.setTextAppearance( context, android.R.style.TextAppearance_Large );
+		action.setLayoutParams( new LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 1 ) );
+		
+		this.addView( action );
+		
+		ImageView remove = new ImageView( context );
+		
+		remove.setImageResource( R.drawable.check );
+		remove.setLayoutParams( new LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT ) );
+		remove.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				((LinearLayout) v.getParent().getParent()).removeView( (View) v.getParent() );
+				
+			}
+		});
+		
+		this.addView( remove );
+	}
+
+}
